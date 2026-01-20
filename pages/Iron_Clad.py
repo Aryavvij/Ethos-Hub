2e57e6340741b0b93c8e041037843d4022c4b48c
Aryavvij <aryavvij@gmail.com>
2026-01-20 18:45:14 +0530

    Update Iron_Clad.py

--- pages/Iron_Clad.py
+++ pages/Iron_Clad.py
@@ -84,7 +84,7 @@ all_ex_df = pd.DataFrame(all_ex_data, columns=["Exercise", "Group", "Prev Kg", "
 updated_sessions = []
 
 for group in muscle_groups:
-    with st.expander(f"➔ {group.upper()} Progress", expanded=(group == "Abs")):
+    with st.expander(f"➔ {group.upper()} PROGRESS", expanded=(group == "Abs")):
         group_df = all_ex_df[all_ex_df["Group"] == group].copy()
         
         if group_df.empty:
