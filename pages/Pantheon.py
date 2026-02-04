e133949b8b8dbd072140ac6d22ea68b0b2bde0f6
Aryavvij <aryavvij@gmail.com>
2026-02-04 09:42:17 +0530

    Update Pantheon.py

--- pages/Pantheon.py
+++ pages/Pantheon.py
@@ -7,7 +7,7 @@ from utils import render_sidebar
 st.set_page_config(layout="wide", page_title="The Pantheon", page_icon="🏛️")
 
 if 'logged_in' not in st.session_state or not st.session_state.logged_in:
-    st.warning("Please log in on the Home page.")
+    st.switch_page("Home.py") 
     st.stop()
 
 render_sidebar()
